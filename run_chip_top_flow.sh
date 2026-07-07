ef54b3620700408178043fe70b23202d6ccaf37c
James <jashiekotey@gmail.com>
2026-07-07 02:25:22 +0100

    rename chip top run script

--- run_chip_top_flow.sh
+++ run_chip_top_flow.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 # https://github.com/splinedrive/gf180mcu-kianv-rv32ima-sv32/blob/main/librelane/config.yaml
 
 PDK_NAME=gf180mcuD
+
 #STD_CELL_LIB=gf180mcu_fd_sc_mcu7t5v0
 STD_CELL_LIB=gf180mcu_fd_sc_mcu9t5v0
 
