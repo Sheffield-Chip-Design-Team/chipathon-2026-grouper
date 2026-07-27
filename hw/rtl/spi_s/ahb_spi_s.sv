23a51b707c3d4268e76270363eacb46dcccd680c
thiri <tsint1@sheffield.ac.uk>
2026-07-27 13:14:08 +0630

    Replace UART register map with SPI register map

--- hw/rtl/spi_s/ahb_spi_s.sv
+++ hw/rtl/spi_s/ahb_spi_s.sv
@@ -29,19 +29,15 @@ module ahb_spi_s #(
   input logic                   HREADYIN,
   input logic                   HSEL,
 
-  // Interrupts
-  output logic                  rx_irq,
-  output logic                  rx_error_irq,
-
-  // UART interface
-  output logic                  uart_tx,
-  input  logic                  uart_rx
+  //SPI interface
+  input logic                   spi_ss,
+  input logic                   spi_sck,
+  input logic                   spi_mosi,
+  output logic                  spi_miso
 );
 
   import ahb3lite_pkg::*;
-
-  localparam int CLK_DIV_BITS = 10;
-  localparam int UART_DATA_W = 8;
+  localparam int SPI_S_DATA_W = 8;
 
   // AHB transfer codes needed in this module
   localparam bit [1:0] No_Transfer  = 2'b00;
@@ -54,33 +50,15 @@ module ahb_spi_s #(
   
   // Control registers
   logic                     ctrl_enable;
-  logic [CLK_DIV_BITS-1:0]  ctrl_clk_div;
-  logic                     ctrl_tx_en;
-  logic                     ctrl_rx_en;
-  logic                     ctrl_rx_resync_en;
-  logic                     ctrl_tx_break;
-  logic                     ctrl_flush_tx_fifo; // WOSC
-  logic                     ctrl_flush_rx_fifo; // WOSC
-
+  logic                     ctrl_soft_reset;
   
   // Status registers
-  logic                     status_tx_empty;
-  logic                     status_tx_full;
-  logic                     status_rx_empty;
-  logic                     status_rx_full;
-  logic                     status_tx_active;
-  logic                     status_rx_frame_error; // RC
-  logic                     status_rx_break; // RC
+  logic                     status_busy;
+  logic                     status_rx_valid;
   
-  // Control signals
-  logic                     rx_frame_error; // 1-cycle pulse on frame error
-  logic                     rx_break;       // High when in a break condition
-
   // Fifo signals
-  logic [UART_DATA_W-1:0]   tx_data;
-  logic                     tx_write;
-  logic [UART_DATA_W-1:0]   rx_data;
-  logic                     rx_read;
+  logic [SPI_S_DATA_W-1:0]   tx_data;
+  logic [SPI_S_DATA_W-1:0]   rx_data;
 
   //control signals are stored in registers
   logic                       access;
@@ -94,9 +72,9 @@ module ahb_spi_s #(
   logic invalid_access;
 
   // Instance the uart core
-  uart #(
+  /*uart #(
     .CLK_DIV_BITS (CLK_DIV_BITS),
-    .DATA_WIDTH   (UART_DATA_W)
+    .DATA_WIDTH   (SPI_S_DATA_W)
   ) u_uart (
       .clk            (HCLK),
       .rst_n          (HRESETn),
@@ -123,17 +101,15 @@ module ahb_spi_s #(
       .uart_tx        (uart_tx),
       .uart_rx        (uart_rx)
   );
+*/
 
-  assign rx_error_irq = rx_frame_error;
 
   //Generate the control signals in the address phase
-  assign access       = HREADYIN && HSEL && (HTRANS != HTRANS_IDLE);
+  assign access       = HREADYIN && HSEL && (HTRANS != No_Transfer);
   assign read_enable  = access && ~HWRITE;
 
   assign word_address = access ? HADDR[3:2] : '0;
   assign byte_select  = access ? generate_byte_select_32(HSIZE, HADDR[1:0]) : '0;
-
-  assign rx_read = read_enable && !status_rx_empty && word_address == ADDR_RXDATA;
 
   // Delay write control signals to data phase
   always_ff @(posedge HCLK, negedge HRESETn)
@@ -155,41 +131,28 @@ module ahb_spi_s #(
   always_ff @(posedge HCLK, negedge HRESETn)
     if (~HRESETn) begin
       ctrl_enable         <= '0;
-      ctrl_clk_div        <= '1;
-      ctrl_tx_en          <= '0;
-      ctrl_rx_en          <= '0;
-      ctrl_rx_resync_en   <= '1;
-      ctrl_tx_break       <= '0;
-      ctrl_flush_tx_fifo  <= '0;
-      ctrl_flush_rx_fifo  <= '0;
+      ctrl_soft_reset         <= '0;
+      tx_data        <= '0;
     end else begin
-      ctrl_flush_tx_fifo <= '0; // Clear after 1 clock cycle
-      ctrl_flush_rx_fifo <= '0; // Clear after 1 clock cycle
+
       if (write_enable)
         unique case (word_address_r)
           ADDR_CTRL: begin
             if (byte_select_r[0]) begin
               ctrl_enable         <= HWDATA[0];
-              ctrl_tx_en          <= HWDATA[1];
-              ctrl_rx_en          <= HWDATA[2];
-              ctrl_rx_resync_en   <= HWDATA[3];
-              ctrl_tx_break       <= HWDATA[4];
-              ctrl_flush_tx_fifo  <= HWDATA[5];
-              ctrl_flush_rx_fifo  <= HWDATA[6];
-              ctrl_clk_div        <= HWDATA[16 +: CLK_DIV_BITS];
+              ctrl_soft_reset     <= HWDATA[1];
             end
           end
+
+          ADDR_TXDATA: begin
+            tx_data <= HWDATA[7:0];
+          end
+
           default: begin end
         endcase
     end
 
-  // Take the ceil of data width / 8
-  assign tx_write = !status_tx_full && (
-    write_enable && word_address_r == ADDR_TXDATA && &(byte_select_r[0 +: (UART_DATA_W+7)/8])
-  );
-  assign tx_data  = HWDATA[0 +: UART_DATA_W];
-
-  // read
+  // read registers
   always_comb
     if (!read_enable_r)
       // (output of zero when not enabled for read is not necessary
@@ -197,48 +160,32 @@ module ahb_spi_s #(
       HRDATA = '0;
     else
       unique case (word_address_r)
-        ADDR_CTRL: HRDATA = {
-          {(16-CLK_DIV_BITS){1'b0}},
-          ctrl_clk_div,
-          11'b0,
-          ctrl_tx_break,
-          ctrl_rx_resync_en,
-          ctrl_rx_en,
-          ctrl_tx_en,
+      
+        // CTRL register (0x00)
+        ADDR_CTRL:
+        HRDATA = { 
+          30'b0,
+          ctrl_soft_reset,
           ctrl_enable
         };
-        ADDR_STATUS: HRDATA = {
-          25'b0,
-          status_rx_break,
-          status_rx_frame_error,
-          status_tx_active,
-          status_rx_full,
-          status_rx_empty,
-          status_tx_full,
-          status_tx_empty
+        
+        // STATUS register (0x04)
+        ADDR_STATUS: 
+        HRDATA = {
+          30'b0,
+          status_rx_valid,
+          status_busy
         };
-        ADDR_RXDATA: HRDATA = {
-          {(32-UART_DATA_W){1'b0}},
+          
+        // RXDATA register (0x0C)
+        ADDR_RXDATA:
+        HRDATA = {
+          24'b0,
           rx_data
         };
+            
         default: HRDATA = '0;
       endcase
-
-  always_ff @(posedge HCLK, negedge HRESETn)
-    if (~HRESETn) begin
-      status_rx_frame_error <= '0;
-      status_rx_break <= '0;
-    end else begin
-      if (read_enable_r && word_address_r == ADDR_STATUS && byte_select_r[0]) begin
-        status_rx_frame_error <= '0;
-        status_rx_frame_error <= '0;
-      end
-
-      if (rx_frame_error)
-        status_rx_frame_error <= '1;
-      if (rx_break)
-        status_rx_break <= '1;
-    end
 
   //Transfer Response
   always_comb begin
@@ -246,20 +193,14 @@ module ahb_spi_s #(
 
     if (write_enable)
       unique case (word_address_r)
-        ADDR_STATUS: invalid_access |= '1;
-        ADDR_TXDATA: invalid_access |= status_tx_full;
-        ADDR_RXDATA: invalid_access |= '1;
-        default: begin end
-      endcase
-
-    if (read_enable)
-      unique case (word_address_r)
-        ADDR_RXDATA: invalid_access |= status_rx_empty;
+        ADDR_STATUS: invalid_access |= '1; //STATUS is read-only
+        ADDR_RXDATA: invalid_access |= '1; //RXDATA is read-only
         default: begin end
       endcase
   end
 
-  assign HREADYOUT = '1; // Single cycle Write & Read. Zero Wait state operations
+  // Single cycle Write & Read. Zero Wait state operations
+  assign HREADYOUT = '1;
 
   // FIXME - add 2-cycle error response for invalid access.
   assign HRESP = invalid_access ? 1'b1 : 1'b0;
