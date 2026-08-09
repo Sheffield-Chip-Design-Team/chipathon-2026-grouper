1ca874c22b4468b25a052c6c264b1d8ff9baa0eb
James <Jashiekotey@gmail.com>
2026-08-09 17:03:51 +0100

    add review notes

--- hw/tb/spi_m/test_spi_m.py
+++ hw/tb/spi_m/test_spi_m.py
@@ -1,7 +1,8 @@
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, Timer, FallingEdge
-
+import logging
+log = logging.getLogger("cocotb.spi_m_tb")
 
 
 # ------------------------------------------------------------
@@ -15,10 +16,8 @@ INT    = 0x0C
 ADDR   = 0x10
 DATA   = 0x14
 
-
 async def reset_dut(dut):
     dut.HRESETn.value = 0
-
     dut.HSEL.value = 0
     dut.HREADYIN.value = 1
     dut.HTRANS.value = 0
@@ -39,6 +38,8 @@ async def reset_dut(dut):
     await RisingEdge(dut.HCLK)
 
 # FIXME - use the common functions for ahb_read/write for consistency
+# These functions don't consider the AHB response (HRESP, HREADYOUT) signals, which is important for error handling.
+  
 async def ahb_write(dut, addr, data):
     # Address phase
     dut.HSEL.value = 1
@@ -104,52 +105,63 @@ async def wait_for_spi_clock(dut, timeout_ns=5000):
 
     return False
 
-# FIXEME - this should be split into multiple tests so that we can reproduce one failure at a time.
-@cocotb.test()
-async def test_ahb_spi_master(dut):
-
-    print("\n========================================")
-    print(" AHB SPI MASTER FULL TEST")
-    print("========================================")
-
-    # Clock
+async def init_test(dut):
+    """Initialize the DUT to a known state."""
     cocotb.start_soon(
         Clock(dut.HCLK, 10, unit="ns").start()
     )
+    await reset_dut(dut)
 
+# FIXME - this should be split into multiple tests so that we can reproduce one failure at a time.
+@cocotb.test()
+async def test_ahb_spi_m_reset(dut):
+    log.info("\n========================================")
+    log.info(" AHB SPI MASTER TEST")
+    log.info("========================================")
+
+    
     # --------------------------------------------------------
     # 1. RESET
     # --------------------------------------------------------
 
-    await reset_dut(dut)
+    await init_test(dut)
+ 
+    # FIXME - there is no check here - we should actually read the registers and check they are reset to default values
+    
+    # FIXME - do NOT use print for logging - use cocotb.log.info() or cocotb.log.debug() instead
+    # print("[PASS] Reset completed") # bad
+    log.info("[PASS] Reset completed") # good
 
-    #FIXME - there is no check here - we should actually read the registers and check they are reset to default values
-    print("[PASS] Reset completed")
 
+@cocotb.test()
+async def test_reg_access(dut):
+    await init_test(dut)
+    
     # --------------------------------------------------------
     # 2. CTRL REGISTER
     # --------------------------------------------------------
 
-    # CPHA = 0
-    # CPOL = 0
-    # CLK_DIV = 4
-    # IE_DONE = 0
-    # IE_ERR = 0
-    #
+    # FIXME - don't use all caps for non-constants - use lower case for local variables instead
+    CPHA = 0
+    CPOL = 0
+    CLK_DIV = 4
+    IE_DONE = 0
+    IE_ERR = 0
+
     # ctrl_clk_div is bits [9:2]
+
+    # FIXME - don't use 'magic numbers' - use the defined constants for the register bit fields instead
+    # maybe use a function like ctrl_word() in test_uart to build the value instead of doing it manually here?
     ctrl_value = (4 << 2)
 
     await ahb_write(dut, CTRL, ctrl_value)
-
     ctrl_read = await ahb_read(dut, CTRL)
-
     print(f"CTRL = 0x{ctrl_read:08X}")
 
     assert (ctrl_read & 0x3FF) == ctrl_value, \
         f"CTRL mismatch: expected 0x{ctrl_value:08X}, got 0x{ctrl_read:08X}"
 
     print("[PASS] CTRL write/read")
-
 
     await ahb_write(dut, CTRL, ctrl_value)
     for _ in range(5):
@@ -186,22 +198,13 @@ async def test_ahb_spi_master(dut):
 
     print("[PASS] STATUS idle state")
 
-    # --------------------------------------------------------
-    # 5. TX FIFO DATA WRITE
-    # --------------------------------------------------------
+  # --------------------------------------------------------
+  # 6. CMD REGISTER
+  # --------------------------------------------------------
+    # FIXME - this should be handled in a function
 
-    tx_data = 0x000000A5
-
-    await ahb_write(dut, DATA, tx_data)
-
-    print("[PASS] DATA write")
-
-    # --------------------------------------------------------
-    # 6. CMD REGISTER
-    # --------------------------------------------------------
-    #
     # CMD layout:
-    #
+    
     # bit 0       START
     # bits 8:1    OPCODE
     # bit 9       CMD_EN
@@ -211,15 +214,14 @@ async def test_ahb_spi_master(dut):
     # bit 14      DIR
     # bits 19:15  DUMMY
     # bits 27:20  LEN
-    #
+    
     # Send:
     # opcode = 0x9A
     # command enabled
     # data enabled
     # write direction
     # length = 1 byte
-    #
-
+    
     opcode = 0x9A
 
     cmd_value = (
@@ -238,11 +240,63 @@ async def test_ahb_spi_master(dut):
 
     await ahb_write(dut, CMD, cmd_value)
 
-    # FIXME - we aren't actually asserting anything here,
-    # using the pre-existing functions means that we can check HRESP
+    # FIXME - we aren't actually asserting anything here
 
     print("[PASS] CMD write")
 
+@cocotb.test()
+async def test_invalid_write(dut):
+    await init_test(dut)
+    
+    # --------------------------------------------------------
+    # 12. INVALID ACCESS
+    # --------------------------------------------------------
+
+    # address phase
+    dut.HSEL.value = 1
+    dut.HREADYIN.value = 1
+    dut.HTRANS.value = 2
+    dut.HWRITE.value = 1
+    dut.HADDR.value = STATUS
+    dut.HSIZE.value = 2
+    dut.HWDATA.value = 0x12345678
+
+    await RisingEdge(dut.HCLK)
+
+    # data phase — HRESP is valid here
+    dut.HSEL.value = 0
+    dut.HTRANS.value = 0
+    dut.HWRITE.value = 0
+
+    await Timer(1, unit="ns")
+
+    assert int(dut.HRESP.value) == 1, \
+        "Invalid STATUS write did not produce HRESP"
+
+    await RisingEdge(dut.HCLK)
+
+@cocotb.test()
+async def test_fifo_write(dut):
+    await init_test(dut)
+    # --------------------------------------------------------
+    # 5. TX FIFO DATA WRITE
+    # --------------------------------------------------------
+
+    tx_data = 0x000000A5
+
+    await ahb_write(dut, DATA, tx_data)
+
+    # FIXME - readback to actually CHECK that it worked?
+    print("[PASS] DATA write")
+
+
+@cocotb.test()
+async def test_fifo_spi_write(dut):
+    await init_test(dut)
+
+    # TODO  - configure the SPI ctrl register
+    # TODO  - write to the DATA register
+    
     # --------------------------------------------------------
     # 7. CHECK SPI CS
     # --------------------------------------------------------
@@ -275,6 +329,8 @@ async def test_ahb_spi_master(dut):
     # --------------------------------------------------------
     # 9. CHECK MOSI ACTIVITY
     # --------------------------------------------------------
+
+    # FIXME - this test does not actually check the VALUE of the data coming out of MOSI
 
     mosi_values = []
 
@@ -333,38 +389,9 @@ async def test_ahb_spi_master(dut):
         print("[INFO] DONE interrupt flag not set")
 
     # --------------------------------------------------------
-    # 12. INVALID ACCESS
-    # --------------------------------------------------------
-
-        # address phase
-    dut.HSEL.value = 1
-    dut.HREADYIN.value = 1
-    dut.HTRANS.value = 2
-    dut.HWRITE.value = 1
-    dut.HADDR.value = STATUS
-    dut.HSIZE.value = 2
-    dut.HWDATA.value = 0x12345678
-
-    await RisingEdge(dut.HCLK)
-
-    # data phase — HRESP is valid here
-    dut.HSEL.value = 0
-    dut.HTRANS.value = 0
-    dut.HWRITE.value = 0
-
-    await Timer(1, unit="ns")
-
-    assert int(dut.HRESP.value) == 1, \
-        "Invalid STATUS write did not produce HRESP"
-
-    await RisingEdge(dut.HCLK)
-
-    # --------------------------------------------------------
     # FINISHED
     # --------------------------------------------------------
 
-    print("\n========================================")
-    print(" ALL AHB SPI MASTER TESTS COMPLETED")
-    print("========================================\n")
-
-    await Timer(20, unit="ns")
+    log.info("\n========================================")
+    log.info(" ALL AHB SPI MASTER TESTS COMPLETED")
+    log.info("========================================\n")
