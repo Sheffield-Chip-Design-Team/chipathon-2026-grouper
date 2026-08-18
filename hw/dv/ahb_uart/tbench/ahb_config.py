1f90bdcd1e6580eb4e2444501a58725ccabcf27f
James <Jashiekotey@gmail.com>
2026-08-18 14:59:54 +0100

    Create virutal sequencer for AHB UART

--- hw/dv/ahb_uart/tbench/ahb_config.py
+++ hw/dv/ahb_uart/tbench/ahb_config.py
@@ -2,7 +2,5 @@ from dataclasses import dataclass
 
 @dataclass
 class AhbConfig:
-    """Top-level bus clock this testbench drives on HCLK. Separate from
-    UartConfig (hw/dv/uvc/uart/uart_config.py), which is clock-independent
-    and only cares about baud_rate/bit_period_ns."""
+    """Top-level bus clock this testbench drives on HCLK."""
     clk_period_ns: int = 10
